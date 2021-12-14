e09f3f88194818658df9707ececb45879cc6733e
kavin <kavin256@gmail.com>
2021-12-15 00:09:46 +0530

    overloading

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -5,15 +5,25 @@ package lectures.part2oop
  * @Date: 2021-12-13
  */
 object OOBasics extends App {
-  val person = new Person ("John", 25)
+  val person = new Person("John", 25)
   println(person.x)
+  person.greet("Daniel")
+  person.greet()
 }
 
 // constructor
-class Person (name: String, val age: Int) {
+class Person(name: String, val age: Int) {
   //body
   val x = 2 // field
-  println(1+3)
+  println(1 + 3)
+
+  // method
+  // here, the important thing is that even though the name is not a field
+  // But it is still available withing the class definition
+  def greet(name: String): Unit = println(s"${this.name} says: Hi, $name")
+
+  // overloading
+  def greet(): Unit = println(s"Hi, I am $name")
 }
 
 // class parameters are not fields
